8cc188a08e249bbcab98406bed5632c06fae6865
gshaurya <gshaurya@umich.edu>
2021-09-17 16:21:46 -0400

    New help command

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,8 @@
+const config = require('../config.json');
 module.exports = {
 	name: 'help',
 	description: 'Summarizes usage of commands supported by the bot',
 	execute(message) {
-		message.reply('TODO.');
+		message.channel.send(`Please attach an image and use \`${config.prefix}resize NEW_WIDTH [NEW_HEIGHT]\``);
 	},
 };
