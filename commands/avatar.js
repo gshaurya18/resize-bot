d1f4e3a5467d0880a218f80eb9b1236049b9a545
gshaurya <gshaurya@umich.edu>
2022-07-20 15:27:11 -0500

    avatar v14 update

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,11 +1,11 @@
-const Discord = require('discord.js');
+const { AttachmentBuilder } = require('discord.js');
 module.exports = {
 	name: 'avatar',
 	description: 'Sends a picture of the user\'s avatar',
 	usage: 'avatar',
 	execute(message) {
 		const url = message.author.displayAvatarURL({ format: 'jpeg' });
-		const attachment = new Discord.MessageAttachment(url);
-		message.channel.send(attachment);
+		const attachment = new AttachmentBuilder(url);
+		message.channel.send({ files: [attachment] });
 	},
 };
